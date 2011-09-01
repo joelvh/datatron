0a718e1b6d5bfa8e624f8bc713c26c313d39026a
Stephen Prater (@dev-env) <stephenp@agrussell.com>
2011-09-01 17:15:21 -0500

    updated termhelper

--- lib/datatron/converter.rb
+++ lib/datatron/converter.rb
@@ -24,58 +24,53 @@ module Datatron
 
     def build_cache
       super
-     
 
-      class << self 
-        def progress_line percentage
-          total_cols = size[:cols].to_i * 0.8
-          cols = percentage.nan? ? 0 : (percentage * (total_cols - 16)).to_i
-
-          output do |str|
-            str << (percentage.nan? ? "  Unknown" : "%8d%%" % (percentage * 100))
-            str << symbols do |str|
-              str << color(:white) do |str| 
-                str << "x"
-              end
-              str << color(:cyan) do |str|
-                str << "a" * cols 
-              end
-              str << c.mrcup(0,total_cols - cols)
-              str << color(:white) do |str|
-                str << "x"
-              end
-            end
-            str << c.column(1)
+      macro :progress_display do |strategy|
+        output do |str|
+          str << color(:yellow) do |str|
+            str << strategy.strategy.base_name
           end
-        end
-
-        def current_strategy strategy
-          output do |str|
-            str << color(:yellow) do |str|
-              str << strategy.strategy.base_name 
-            end
-            str << " #{strategy.progress[:successful]} out of #{strategy.progress[:seen]}   "
-            str << color(:red) do |str|
-              str << "#{strategy.progress[:error_count]} Errors -- last error was #{strategy.progress[:last_error]}"
-            end
-            str << c.down
-            str << "Source %"
-            str << progress_line(strategy.progress[:source_percent])
-            str << c.down
-            str << "Dest   %"
-            str << progress_line(strategy.progress[:dest_percent])
-            str << c.up * 2 
+          str << strategy.progress[:successful] + 'out of' +  strategy.progress[:seen]
+          str << color(:red) do |str|
+            str << strategy.progress[:error_count] + "Errors -- last error was " + strategy.progress[:last_error]
           end
+          str << c.down
+          str << "Source %"
+          str << widget.progress_line(strategy.progress[:source_percent])
+          str << c.down
+          str << "Dest   %"
+          str << widget.progress_line(strategy.progress[:dest_percent])
+          str << c.up * 2 
         end
       end
 
-      @cached = true
+      widget :progress_line do |percentage|
+        total_cols = size[:cols].to_i * 0.8
+        cols = percentage.nan? ? 0 : (percentage * (total_cols - 16)).to_i
+
+        output do |str|
+          str << (percentage.nan? ? "  Unknown" : "%8d%%" % (percentage * 100))
+          str << symbols do |str|
+            str << color(:white) do |str| 
+              str << "x"
+            end
+            str << color(:cyan) do |str|
+              str << "a" * cols 
+            end
+            str << c.mrcup(0,total_cols - cols)
+            str << color(:white) do |str|
+              str << "x"
+            end
+          end
+          str << c.column(1)
+        end
+      end
     end
 
     def update translator
-      build_cache unless @cached
+      build_cache 
       output STDOUT do |str|
-        str << current_strategy(translator)
+        str << c.progress_display(translator)
       end
     end
 
